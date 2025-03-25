c5df5bfaf87c2281b3807023c9e2d9eed01b4987
girlanasouza <girlanasouza123@gmail.com>
2025-03-24 22:44:12 -0300

    Refactor Immediate Extension module to use instruction opcode for immediate value extraction and update testbench for improved monitoring and test cases

--- src/imm_ext.v
+++ src/imm_ext.v
@@ -1,34 +1,46 @@
 module ImmExt (
     input [31:0] instruction,
-    input [2:0] imm_src,
     output reg [31:0] imm_ext
 );
 
-    always @(*) begin
-        case (imm_src)
-            // I-type: ADDI, LW, JALR, etc.
-            3'b000: 
-                imm_ext = {{20{instruction[31]}}, instruction[31:20]};
-            
-            // S-type: SW, SH, SB
-            3'b001: 
-                imm_ext = {{20{instruction[31]}}, instruction[31:25], instruction[11:7]};
-            
-            // B-type: BEQ, BNE, etc.
-            3'b010: 
-                imm_ext = {{19{instruction[31]}}, instruction[31], instruction[7], instruction[30:25], instruction[11:8], 1'b0};
-            
-            // U-type: LUI, AUIPC
-            3'b011: 
-                imm_ext = {instruction[31:12], 12'b0};
-            
-            // J-type: JAL
-            3'b100: 
-                imm_ext = {{11{instruction[31]}}, instruction[31], instruction[19:12], instruction[20], instruction[30:21], 1'b0};  
+    localparam [6:0]
+        LW_OP = 7'b0000011,
+        SW_OP =  7'b0100011,
+        JAL_OP = 7'b1101111,
+        LUI_OP = 7'b0110111,
+        CSR_OP = 7'b1110011,
+        JALR_OP = 7'b1100111,
+        AUIPC_OP = 7'b0010111,
+        BEQ_OP = 7'b1100011,
+        IMM_OP = 7'b0010011;
 
-            // Default: Zero (ou I-type)
+    always @(*) begin
+        case (instruction[6:0])
+            BEQ_OP: // SB type
+                imm_ext = {{19{instruction[31]}}, instruction[31], instruction[7], instruction[30:25], instruction[11:8], 1'b0};
+            JAL_OP: // UJ type JAL
+                imm_ext = {{11{instruction[31]}}, instruction[31], instruction[19:12], instruction[20], instruction[30:21], 1'b0};
+            AUIPC_OP: // AUIPC U type
+                imm_ext = {instruction[31:12], 12'h000};
+            LUI_OP: // LUI U type
+                imm_ext = {instruction[31:12], 12'h000};
+            LW_OP: // lw instruction 
+                imm_ext = {{20{instruction[31]}}, instruction[31:20]};
+            IMM_OP: // I type instruction
+                case (instruction[14:12])
+                    3'b001: imm_ext = {{27{instruction[24]}}, instruction[24:20]};
+                    3'b011: imm_ext = {20'h00000, instruction[31:20]};
+                    3'b101: imm_ext = {{27'h0000000}, instruction[24:20]};
+                    default: imm_ext = {{20{instruction[31]}}, instruction[31:20]};
+                endcase
+            JALR_OP: // I type instruction JALR
+                imm_ext = {{20{instruction[31]}}, instruction[31:20]};
+            CSR_OP: // I type instruction  CSR
+                imm_ext = {20'h00000, instruction[31:20]};
+            SW_OP: // sw instruction  (S type)
+                imm_ext = {{20{instruction[31]}}, instruction[31:25], instruction[11:7]};
             default: 
-                imm_ext = 32'b0;
+                imm_ext = 32'h00000000;
         endcase
     end
 
--- testbench/tb_imm_ext.v
+++ testbench/tb_imm_ext.v
@@ -1,56 +1,46 @@
 module ImmExtTb;
     reg [31:0] instruction;
-    reg [2:0] imm_src;
-
     wire [31:0] imm_ext;
 
     ImmExt uut (
         .instruction(instruction),
-        .imm_src(imm_src),
         .imm_ext(imm_ext)
     );
 
     initial begin
-        $monitor("Time: %0t | imm_src: %b | imm_ext: %h", $time, imm_src, imm_ext);
+        $monitor("Time: %0t | instruction: %h | imm_ext: %h\n", $time, instruction, imm_ext);
 
         // Test 1: I-type (ADDI)
         instruction = 32'hFFF00013; // ADDI x0, x0, -1
-        imm_src = 3'b000;
         #10;
         if (imm_ext !== 32'hFFFFFFFF) $error("Test 1 Failed");
         else $display("Test 1 Passed %h\n", imm_ext);
 
         // Test 2: S-type (SW)
-        instruction = 32'h0064A423; // sw x6, 8(x9)
-        imm_src = 3'b001;
+        instruction = 32'h0064A423; // SW x6, 8(x9)
         #10;
-        if (imm_ext !== 32'h00000008) $display("Test 2 Failed %h", imm_ext);
-        $display("\n");
+        if (imm_ext !== 32'h00000008) $error("Test 2 Failed %h", imm_ext);
+        else $display("Test 2 Passed %h\n", imm_ext);
 
         // Test 3: B-type (BEQ)
-        instruction = 32'hFE420AE3; // beq x4, x4, L7
-        imm_src = 3'b010;
+        instruction = 32'hFE420AE3; // BEQ x4, x4, L7
         #10;
-        if (imm_ext !== 32'hFFFFFFF4) $display("Test 3 Failed %h", imm_ext);
-        $display("\n");
+        if (imm_ext !== 32'hFFFFFFF4) $error("Test 3 Failed %h", imm_ext);
+        else $display("Test 3 Passed %h\n", imm_ext);
 
         // Test 4: U-type (LUI)
         instruction = 32'h12345037; // LUI x0, 0x12345
-        imm_src = 3'b011;
         #10;
-        if (imm_ext !== 32'h12345000) $error("Test 4 Failed");
-        $display("\n");
+        if (imm_ext !== 32'h12345000) $error("Test 4 Failed %h", imm_ext);
+        else $display("Test 4 Passed %h\n", imm_ext);
 
         // Test 5: J-type (JAL)
-        //instruction = 32'h12345FEF; // JAL x31, 0x12345
-        instruction = 32'h7F8A60EF; // jal x1, 0xA67F8
-        imm_src = 3'b100;
+        instruction = 32'h7F8A60EF; // JAL x1, 0xA67F8
         #10;
-        if (imm_ext !== 32'hA67F8) $error("Test 5 Failed");
-        $display("\n");
+        if (imm_ext !== 32'h00A67F8) $error("Test 5 Failed %h", imm_ext);
+        else $display("Test 5 Passed %h\n", imm_ext);
 
-        
-        $display("All tests passed!");
+        $display("All tests completed!");
         $finish;
     end
 
