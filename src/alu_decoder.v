3272f5076a980d6e59a98b51eced31fc092b8c20
girlanasouza <girlanasouza123@gmail.com>
2025-03-26 10:08:39 -0300

    Refactor ALUDecoder module to simplify control signal logic and improve handling of ALU operations; remove unused cases and streamline output assignments

--- src/alu_decoder.v
+++ src/alu_decoder.v
@@ -1,5 +1,4 @@
-module ALUDecoder(
-    input is_imm,              
+module ALUDecoder(            
     input [6:0] funct7,  
     input [2:0] funct3,         
     input [1:0] alu_op,        
@@ -17,26 +16,16 @@ module ALUDecoder(
         SRA  = 4'b0011,  // SRA, SRAI
         OR   = 4'b0001,  // OR, ORI
         AND  = 4'b0000,  // AND, ANDI
-        EQ   = 4'b1110,  // BEQ
-        GE   = 4'b1011,  // BGE
-        GEU  = 4'b1101;  // BGEU
+
 
     always @(*) begin
 
         case (alu_op)
             alu_out = ADD;
-            2'b00: alu_out = 4'b0010; 
-            2'b01: begin
-                case (funct3)
-                    3'b000: alu_out = SUB; 
-                    3'b001: alu_out = EQ;
-                    3'b100: alu_out = GE;
-                    3'b110: alu_out = GEU;
-                    3'b101: alu_out = SLT;
-                    3'b111: alu_out = SLTU;
-                    default: alu_out = SUB; // Default (SUB)
-                endcase
-            end
+            2'b00: 
+                alu_out = ADD; 
+            2'b01:  // beq 
+                alu_out = SUB; 
             
             2'b10: begin // R-type instructions
                 case (funct3)
@@ -46,6 +35,7 @@ module ALUDecoder(
                     3'b011: alu_out = SLTU; 
                     3'b100: alu_out = XOR;
                     3'b101: alu_out = (funct7[5] == 1'b1) ? SRA : SRL;
+                    
                     3'b110: alu_out = OR;
                     3'b111: alu_out = AND;
                     default: alu_out = ADD; // Default (ADD)
